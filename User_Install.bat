0b54f577c4c7b5396df4b13d20cc6a7fb70f9f8a
Alex N. <167883832+alreedtv@users.noreply.github.com>
2024-12-24 09:08:08 +0300

    Update User_Install.bat

--- User_Install.bat
+++ User_Install.bat
@@ -1,16 +1,16 @@
 @echo off
-@echo  Установщик SUPERSANEK rev. 1.92_04.24 (Apr 2024) для ОПС запущен.
-@echo  Пакет:
-powershell write-host -BackgroundColor White -ForegroundColor Black Яндекс Браузер
+@echo  тАЬ├б├в┬а┬н┬о┬в├й┬и┬к SUPERSANEK rev. 1.92_04.24 (Apr 2024) ┬д┬л├п ┼╜┬ПтАШ ┬з┬а┬п├г├й┬е┬н.
+@echo  ┬П┬а┬к┬е├в:
+powershell write-host -BackgroundColor White -ForegroundColor Black ┼╕┬н┬д┬е┬к├б ┬Б├а┬а├г┬з┬е├а
 powershell write-host -BackgroundColor Yellow -ForegroundColor Black ZOOM Meetings
 @pause
 mkdir %TEMP%\Install
-powershell write-host -BackgroundColor White -ForegroundColor Black --Яндекс Браузер
-winget install "Yandex Browser" -s winget
+powershell write-host -BackgroundColor White -ForegroundColor Black --┼╕┬н┬д┬е┬к├б ┬Б├а┬а├г┬з┬е├а
+winget install Yandex -s winget
 powershell write-host -BackgroundColor Cyan -ForegroundColor White --Zoom Workplace 
 winget install "zoom workplace" -s winget
 :choiceDEL
-set /P c=Удалить использованные установочные файлы?(Y/N)
+set /P c=тАЬ┬д┬а┬л┬и├в├м ┬и├б┬п┬о┬л├м┬з┬о┬в┬а┬н┬н├л┬е ├г├б├в┬а┬н┬о┬в┬о├з┬н├л┬е ├д┬а┬й┬л├л?(Y/N)
 if /I "%c%" EQU "Y" goto :installDEL
 if /I "%c%" EQU "N" goto :terminate
 goto :choiceDEL
@@ -18,11 +18,11 @@ goto :choiceDEL
 :installDEl
 del /q/f/s %TEMP%\WinGet\*
 del /q/f/s %TEMP%\Install\*
-powershell write-host -BackgroundColor White -ForegroundColor Black Установочные файлы удалены. Завершение программы...
+powershell write-host -BackgroundColor White -ForegroundColor Black тАЬ├б├в┬а┬н┬о┬в┬о├з┬н├л┬е ├д┬а┬й┬л├л ├г┬д┬а┬л┬е┬н├л. тАб┬а┬в┬е├а├и┬е┬н┬и┬е ┬п├а┬о┬г├а┬а┬м┬м├л...
 goto :terminate
 
 
 :terminate
-powershell write-host -BackgroundColor Yellow -ForegroundColor Black Спасибо, что выбрали ReedTech! Всего доброго!
+powershell write-host -BackgroundColor Yellow -ForegroundColor Black тАШ┬п┬а├б┬и┬б┬о, ├з├в┬о ┬в├л┬б├а┬а┬л┬и ReedTech! тАЪ├б┬е┬г┬о ┬д┬о┬б├а┬о┬г┬о!
 @pause
 exit
